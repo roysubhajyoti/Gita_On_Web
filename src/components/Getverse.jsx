04eba2ee2e76425f26b4d0d9fbb2c79d680715ea
roysubhajyoti <roysubbhajyoti272@gmail.com>
2024-02-19 01:17:10 +0530

    change in verse

--- src/components/Getverse.jsx
+++ src/components/Getverse.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 export const Getverse = ({ data }) => {
   return (
-    <div className="w-[600px] h-[600px] border-8 border-solid rounded-2xl border-transparent/10 flex flex-col gap-5 text-black">
+    <div className="w-[600px] h-[600px] border-8 border-solid rounded-2xl border-transparent/10 flex flex-col gap-5 text-white">
       <div className="w-[570px] h-72 border-4 m-2 rounded-2xl border-transparent/10 backdrop-blur-sm shadow-lg">
         <h1 className="font-bold p-3 text-2xl">{data?.slok}</h1>
         <h1 className="font-bold p-3 text-xl">{data?.transliteration}</h1>
